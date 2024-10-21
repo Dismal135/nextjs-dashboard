6af351034bd21dec4002ddfbf33a67ec70d3d68f
phyo thiha <phyothiha97531@gmail.com>
2024-10-21 20:31:33 +0630

    nextauth

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -3,6 +3,29 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
+ 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
+ 
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
 
 const FormSchema = z.object({
     id: z.string(),
@@ -48,10 +71,14 @@ export async function createInvoice(prevState: State, formData: FormData) {
       const amountInCents = amount * 100;
       const date = new Date().toISOString().split('T')[0];
 
-      await sql`
+      try{await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+  `;}catch (error) {
+    return {
+      message: "Database Error: Failed to Create invoice.",
+    }
+  }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
   }
